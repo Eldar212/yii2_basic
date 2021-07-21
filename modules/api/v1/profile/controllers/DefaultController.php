ec11aa816273a2bbca50e4a3b27f59466436b7b4
eldar <eldar-aliev.2000@mail.ru>
2021-07-21 17:24:48 +0300

    Изменен code style

--- modules/api/v1/profile/controllers/DefaultController.php
+++ modules/api/v1/profile/controllers/DefaultController.php
@@ -1,31 +1,31 @@
 <?php
 
 
-    namespace app\modules\api\v1\profile\controllers;
+namespace app\modules\api\v1\profile\controllers;
 
 
-    use app\modules\api\v1\profile\services\ProfileService;
-    use Yii;
-    use yii\web\Controller;
+use app\modules\api\v1\profile\services\UserProfileService;
+use Yii;
+use yii\web\Controller;
 
-    class DefaultController extends Controller
+class DefaultController extends Controller
+{
+    /**
+     * @var UserProfileService
+     */
+    protected $profileService;
+
+    public function __construct($id, $module, UserProfileService $profileService, $config = [])
     {
-        /**
-         * @var ProfileService
-         */
-        protected $profileService;
+        $this->profileService = $profileService;
 
-        public function __construct($id, $module, ProfileService $profileService, $config = [])
-        {
-            $this->profileService = $profileService;
-
-            parent::__construct($id, $module, $config);
-        }
-
-        public function actionUpdate(): array
-        {
-            $request = Yii::$app->request->bodyParams;
-
-            return $this->profileService->update($request);
-        }
+        parent::__construct($id, $module, $config);
     }
+
+    public function actionUpdate(): array
+    {
+        $request = Yii::$app->request->bodyParams;
+
+        return $this->profileService->update($request);
+    }
+}
